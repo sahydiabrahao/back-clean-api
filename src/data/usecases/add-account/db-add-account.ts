f13c8405de24f5f96c41d2eff59af4e926086572
sahydiabrahao <sahydi@protonmail.com>
2023-02-23 10:35:50 -0400

    test: ensure DbAddAccount throws if Encrypter throws

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -11,6 +11,6 @@ export class DbAddAccount implements AddAccount {
 
   async add (account: AddAccountModel): Promise<AccountModel> {
     await this.encrypter.encrypt(account.password)
-    return new Promise(resolve => { resolve(null); })
+    return new Promise(resolve => resolve(null))
   }
 }
--- src/util/email-validator-adapter.spec.ts
+++ src/util/email-validator-adapter.spec.ts
@@ -12,20 +12,20 @@ const makeSut = (): EmailValidatorAdapter => {
 }
 
 describe('EmailValidator Adapter', () => {
-  test('Deve retornar falso se o VALIDATOR retornar falso', () => {
+  test('Deve retornar falso se o Validator retornar falso', () => {
     const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email')
     expect(isValid).toBe(false)
   })
 
-  test('Deve retornar verdadeiro se o VALIDATOR retornar verdadeiro', () => {
+  test('Deve retornar verdadeiro se o Validator retornar verdadeiro', () => {
     const sut = makeSut()
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(true)
   })
 
-  test('Deve chamar VALIDATOR com o email correto', () => {
+  test('Deve chamar Validator com o email correto', () => {
     const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     sut.isValid('any_email@mail.com')

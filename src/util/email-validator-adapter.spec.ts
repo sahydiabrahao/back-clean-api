a5c52d1621eff554468db1933dec723968613856
sahydiabrahao <sahydi@protonmail.com>
2023-02-23 09:29:50 -0400

    test: ensure EmailValidatorAdapter calls validator with corret email

--- src/util/email-validator-adapter.spec.ts
+++ src/util/email-validator-adapter.spec.ts
@@ -20,4 +20,11 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(true)
   })
+
+  test('Deve chamar VALIDATOR com o email correto', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    sut.isValid('any_email@mail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
 })

343a3f11519c37f958675450c1cd825bb3ece5af
sahydiabrahao <sahydi@protonmail.com>
2023-02-23 09:24:40 -0400

    test: ensure EmailValidatorAdapter return true if validator return true

--- src/util/email-validator-adapter.spec.ts
+++ src/util/email-validator-adapter.spec.ts
@@ -1,9 +1,23 @@
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true
+  }
+}))
 
 describe('EmailValidator Adapter', () => {
   test('Deve retornar falso se o VALIDATOR retornar falso', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@mail.com')
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email')
     expect(isValid).toBe(false)
+  })
+
+  test('Deve retornar verdadeiro se o VALIDATOR retornar verdadeiro', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(true)
   })
 })

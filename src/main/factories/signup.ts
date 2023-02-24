7b53978f3dc8aabeafcd2def08ee60f97e47f304
sahydiabrahao <sahydi@protonmail.com>
2023-02-24 16:57:56 -0400

    feat: add LogMongoRepository to a SignUp Factory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,7 +1,7 @@
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account';
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter';
 import { AccountMongoReository } from '../../infra/db/mongodb/account-repository/Account';
-
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log';
 import { SignUpController } from '../../presentation/controllers/signup/signup';
 import { Controller } from '../../presentation/protocols';
 import { EmailValidatorAdapter } from '../../util/email-validator-adapter';
@@ -14,5 +14,6 @@ export const makeSignUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoReository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(signUpController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }

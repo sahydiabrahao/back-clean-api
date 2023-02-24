0cecf3c548ccf6d61632aaaa52bf369676e05483
sahydiabrahao <sahydi@protonmail.com>
2023-02-24 09:27:52 -0400

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,13 @@
+// import request from 'supertest'
+// import app from '../config/app'
+
+// describe('Content Type Middleware', () => {
+//   test('Deve retornar o conteudo padrao como json', async () => {
+//     app.post('/test_content_type', (req, res) => {
+//       res.send('')
+//     })
+//     await request(app)
+//       .get('/test_content_type')
+//       .expect('content-type', /json/)
+//   })
+// })

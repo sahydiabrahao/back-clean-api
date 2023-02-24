938b06c1f947bd63af5426ecb45263976f9aaf51
sahydiabrahao <sahydi@protonmail.com>
2023-02-24 10:42:44 -0400

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,15 @@
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account';
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter';
+import { AccountMongoReository } from '../../infra/db/mongodb/account-repository/Account';
+
+import { SignUpController } from '../../presentation/controllers/signup/signup';
+import { EmailValidatorAdapter } from '../../util/email-validator-adapter';
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoReository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+}

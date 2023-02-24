01ea3934e164523441e26774317144e4a7daa3a3
sahydiabrahao <sahydi@protonmail.com>
2023-02-23 21:32:25 -0400

    feat: mongo db test fix

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,14 +4,14 @@ export const Mongohelper = {
   client: null as MongoClient,
 
   async connect (url: string): Promise<void> {
-    this.client = await MongoClient.connect(global.__MONGO_URI__, {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
   },
 
-  async disconnect () {
-    await this.client.close()
+  async disconnect (): Promise<void> {
+    await this.client.close(global.__MONGO_URI__)
   },
 
   getCollection (name: string): Collection {

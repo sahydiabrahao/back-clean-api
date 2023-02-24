01e616bfc4d33d92fb99a4082c8c474e1f791dfa
sahydiabrahao <sahydi@protonmail.com>
2023-02-24 13:44:48 -0400

    feat: ensure LogControllerDecorator calls controller method

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,13 +3,16 @@ import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter';
 import { AccountMongoReository } from '../../infra/db/mongodb/account-repository/Account';
 
 import { SignUpController } from '../../presentation/controllers/signup/signup';
+import { Controller } from '../../presentation/protocols';
 import { EmailValidatorAdapter } from '../../util/email-validator-adapter';
+import { LogControllerDecorator } from '../decorators/log';
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoReository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return new LogControllerDecorator(signUpController)
 }

7590a741d1338e25759cdbff7a395c74a42de116
sahydiabrahao <sahydi@protonmail.com>
2023-02-25 14:26:33 -0400

    test: ensure LoginController returns 200 if valid credentials

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from "../../errors"
-import { badRequest, serverError, unauthorized } from "../../helpers/http-helper"
+import { badRequest, serverError, unauthorized, ok } from "../../helpers/http-helper"
 import { EmailValidator, HttpRequest, Authentication } from "./login-protocols"
 import { LoginController } from "./login"
 
@@ -100,7 +100,7 @@ describe('Login Controller', () => {
     expect(authSpy).toHaveBeenCalledWith('any_email@mail.com', 'any_password')
   })
 
-  test('Deve retornar 401 se fornecer credenciais inválidas',async () => {
+  test('Deve retornar 401 se fornecer credenciais invalidas',async () => {
     const { sut, authenticationStub } = makeSut()
     jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve(null)))
     const httpResponse =await sut.handle(makeFakeRequest())
@@ -112,5 +112,11 @@ describe('Login Controller', () => {
     jest.spyOn(authenticationStub, 'auth').mockResolvedValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse =await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Deve retornar 200 se fornecer credenciais validas',async () => {
+    const { sut } = makeSut()
+    const httpResponse =await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token'}))
   })
 })

733afc607871fd106ea1a79046d68935c11c85a8
sahydiabrahao <sahydi@protonmail.com>
2023-02-27 14:18:37 -0400

    test: ensure RequiredFieldValidation not return if validadion success

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('Deve retornar a MissingParamError se validation der erro', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
+  })
 })

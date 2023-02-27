9e840b20e8389a86bf7fa3a2a338de1108ac551c
sahydiabrahao <sahydi@protonmail.com>
2023-02-27 15:42:20 -0400

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized, ok } from '../../helpers/http-helper'
+import { badRequest, serverError, unauthorized, ok } from '../../helpers/http/http-helper'
 import { HttpRequest, Authentication, Validation } from './login-protocols'
 import { LoginController } from './login'
 

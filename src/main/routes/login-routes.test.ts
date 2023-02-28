a5ffd10d9fc5d99ca7eccd3ecdf59a7525e34658
sahydiabrahao <sahydi@protonmail.com>
2023-02-28 15:06:02 -0400

    refactor: rename signup routes to login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import { Mongohelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await Mongohelper.connect(process.env.MONGO_URL)
   })
@@ -15,16 +15,17 @@ describe('SignUp Routes', () => {
     const accountCollection = await Mongohelper.getCollection('accounts')
     await accountCollection.deleteMany()
   })
-
-  test('Deve retornar conta em caso de sucesso', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'João',
-        email: 'joao_email@mail.com',
-        password: '123',
-        passwordConfirmation: '123'
-      })
-      .expect(200)
+  describe('POST /signup', () => {
+    test('Deve retornar 200 no signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'João',
+          email: 'joao_email@mail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
   })
 })

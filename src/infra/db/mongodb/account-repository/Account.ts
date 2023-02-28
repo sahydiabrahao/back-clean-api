c4c51a901e04c8c6fc6a41a22a4726e8965961fc
sahydiabrahao <sahydi@protonmail.com>
2023-02-28 14:10:11 -0400

    feat: ensure AccountMongoRepository return account on sucess

--- src/infra/db/mongodb/account-repository/Account.ts
+++ src/infra/db/mongodb/account-repository/Account.ts
@@ -1,12 +1,19 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository';
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository';
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { Mongohelper } from '../helpers/mongo-helper';
 
-export class AccountMongoReository implements AddAccountRepository {
+export class AccountMongoReository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await Mongohelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     return Mongohelper.map(result.ops[0])
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await Mongohelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    return account && Mongohelper.map(account)
   }
 }

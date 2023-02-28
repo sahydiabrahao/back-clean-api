9b205ba6a22f1ba27ce9bfadb07b49727467affc
sahydiabrahao <sahydi@protonmail.com>
2023-02-28 12:58:42 -0400

    test: ensure BcryptAdapter returns true when bcrypt compare success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -42,4 +42,10 @@ describe('Bcrypt Adapter', () => {
     await sut.compare('any_value', 'any_hash')
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
+
+  test('Deve retornar verdadeieo caso Bcrypt.compare der sucesso', async () => {
+    const sut = makeSut()
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(true)
+  })
 })
